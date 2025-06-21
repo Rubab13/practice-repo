e5055e2050551bb5f6d629435bdf23aaabecce4a
Rubab <ahmadrubab13@gmail.com>
2025-06-21 17:19:31 +0500

    changes

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+import mysql.connector
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return 'Hello Docker compose compose s!'
+    try:
+        connection = mysql.connector.connect(
+            host="db",       # service name from docker-compose
+            user="root",
+            password="password",
+            database="mydb"
+        )
+        return "Connected to MySQL DB!"
+    except Exception as e:
+        return f"Error: {str(e)}"
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
